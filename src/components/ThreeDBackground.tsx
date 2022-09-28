12d2670b3d88023bbf31e281dd9c82ba0bf3d867
ayfor <josh.a.stubbington@gmail.com>
2022-09-27 22:56:35 -0400

    wip 3d objects

--- src/components/ThreeDBackground.tsx
+++ src/components/ThreeDBackground.tsx
@@ -1,14 +1,43 @@
-import { Canvas } from "@react-three/fiber";
+import { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+const Box = () => {
+	const boxRef = useRef();
+	useFrame(() => {
+		boxRef.current.rotation.x += 0.01;
+		boxRef.current.rotation.y += 0.01;
+	});
+
+	return (
+		<mesh ref={boxRef} position={[0, 0, 0]}>
+			<boxBufferGeometry args={[2, 2, 2]} />
+			<meshStandardMaterial color='hotpink' />
+		</mesh>
+	);
+};
+
+const Sphere = () => {
+	const sphereRef = useRef();
+	useFrame(() => {
+		sphereRef.current.rotation.x += 0.01;
+		sphereRef.current.rotation.y += 0.01;
+	});
+
+	return (
+		<mesh ref={sphereRef} position={[2, 2, 2]}>
+			<sphereBufferGeometry args={[2, 2, 2]} />
+			<meshStandardMaterial color='hotpink' />
+		</mesh>
+	);
+};
 
 const ThreeDBackground = () => {
 	return (
 		<Canvas>
 			<ambientLight intensity={0.1} />
 			<directionalLight color='red' position={[0, 0, 5]} />
-			<mesh>
-				<boxGeometry />
-				<meshStandardMaterial />
-			</mesh>
+			<Box />
+			<Sphere />
 		</Canvas>
 	);
 };
