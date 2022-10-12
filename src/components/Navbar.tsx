a3c264a3c2ad5ee31122f8e046dbc52e972fe5fd
ayfor <josh.a.stubbington@gmail.com>
2022-10-12 01:02:54 -0400

    style improvements

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,62 +24,88 @@ export default function Navbar() {
 	const [open, setOpen] = useState(false);
 
 	return (
-		<div className="absolute w-full bg-transparent z-50">
-			<Disclosure as="div" className="sticky top-0">
-				<>
-					<div className="mx-auto max-w-full px-2 sm:px-6 lg:px-8">
-						<div className="relative flex h-16 items-center justify-between my-2">
-							{/* PROFILE CARD */}
-							<div className="absolute bg-white drop-shadow-xl inset-y-0 left-0 flex items-center justify-between xl:justify-start w-80 min-w-fit rounded-md md:inset-auto md:ml-2 md:pr-0">
-								<div className="flex-shrink-0 p-2">
-									{/* Profile Picture */}
-									<div className="flex border-2 border-transparent rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-										<Image height="50%" width="50%" className="h-14 w-14 rounded-full" src="/icon.svg" alt="" />
-									</div>
-								</div>
-								{/* Name */}
-								<div className="px-3 text-black w-fit">
-									<h3 className="p-0 text-baseol font-bold">Joshua Stubbington</h3>
-									<Carousel items={titles} />
-								</div>
-								{/* Mobile menu button*/}
-								<div className="inset-y-0 right-0 flex items-end xl:hidden w-max mx-2">
-									<Disclosure.Button className="inline-flex items-center justify-center rounded-lg p-1 text-gray-400 hover:bg-black hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white  transition ease-in-out duration-200 hover:scale-125">
-										<span className="sr-only">Open main menu</span>
-										{open ? <XMarkIcon className="block h-6 w-6" aria-hidden="true" /> : <Bars3Icon className="block h-6 w-6" aria-hidden="true" />}
-									</Disclosure.Button>
+		<div className='absolute w-full bg-transparent z-50 my-2'>
+			<Disclosure as='div' className='sticky top-0'>
+				<div className='mx-auto max-w-full px-2 sm:px-6 lg:px-8'>
+					<div className='relative flex h-16 items-center justify-between my-2'>
+						{/* PROFILE CARD */}
+						<div className='absolute bg-white drop-shadow-xl inset-y-0 left-0 p-1 flex items-center justify-between xl:justify-start w-80 min-w-fit rounded-lg md:inset-auto md:ml-2 md:pr-0'>
+							<div className='flex-shrink-0 p-2'>
+								{/* Profile Picture */}
+								<div className='flex border-2 border-transparent rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
+									<Image height='50%' width='50%' className='h-24 w-24 rounded-lg' src='/icon.svg' alt='' />
 								</div>
 							</div>
-							{/* Menu */}
-							<div className="flex flex-1 items-center justify-center">
-								<div className="hidden w-4/5 flex-row justify-end items-center sm:ml-6 xl:flex">
-									<div className="w-1/2 flex justify-between space-x-4">
-										{navigation.map((item) => (
-											<button key={item.name} className="text-black font-medium bg-white drop-shadow-md px-3 py-2 rounded-full w-1/3 hover:bg-black hover:text-white transition duration-150 ease-in-out" aria-current={item.current ? "page" : undefined}>
-												{item.name}
-											</button>
-										))}
-									</div>
-								</div>
+							{/* Name */}
+							<div className='px-3 text-black w-fit'>
+								<h3 className='p-0 text-baseol font-bold'>Joshua Stubbington</h3>
+								<Carousel items={titles} />
 							</div>
-							{/* Action */}
-							<div className="absolute hidden inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 xl:block">
-								<div className="flex-shrink-0">
-									<button className="bg-violet-800 px-16 py-3 drop-shadow-2xl rounded-full text-white font-medium hover:text-white hover:animate-pulse transition ease-in-out duration-200 hover:scale-110">Contact Me</button>
+							{/* Mobile menu button*/}
+							<div className='inset-y-0 right-0 flex items-end xl:hidden w-max mx-2'>
+								<Disclosure.Button className='inline-flex items-center justify-center rounded-lg p-1 text-gray-400 hover:bg-black hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white  transition ease-in-out duration-200 hover:scale-125'>
+									<span className='sr-only'>Open main menu</span>
+									{open ? (
+										<svg key='lbcaicon' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6'>
+											<path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12-7.5-7.5' />
+										</svg>
+									) : (
+										<svg
+											key='dbcaicon'
+											xmlns='http://www.w3.org/2000/svg'
+											fill='none'
+											viewBox='0 0 24 24'
+											strokeWidth={1.5}
+											stroke='currentColor'
+											className='w-6 h-6 animate-bounce'
+										>
+											<path strokeLinecap='round' strokeLinejoin='round' d='M19.5 8.25l-7.5 7.5-7.5-7.5' />
+										</svg>
+									)}
+								</Disclosure.Button>
+							</div>
+						</div>
+						{/* Menu */}
+						<div className='flex flex-1 items-center justify-center'>
+							<div className='hidden w-4/5 flex-row justify-end items-center sm:ml-6 xl:flex'>
+								<div className='w-1/2 flex justify-between space-x-4'>
+									{navigation.map((item) => (
+										<button
+											key={item.name}
+											className='text-black font-medium bg-white drop-shadow-md px-3 py-2 rounded-full w-1/3 hover:bg-black hover:text-white transition duration-150 ease-in-out'
+											aria-current={item.current ? "page" : undefined}
+										>
+											{item.name}
+										</button>
+									))}
 								</div>
 							</div>
 						</div>
-						<Disclosure.Panel className="xl:hidden">
-							<div className="space-y-1 px-2 pt-2 pb-3">
-								{navigation.map((item) => (
-									<Disclosure.Button key={item.name} as="a" href={item.href} className="block w-fit px-10 py-2 rounded-lg text-base font-medium bg-white drop-shadow-md hover:bg-black hover:text-white transition duration-150 ease-in-out" aria-current={item.current ? "page" : undefined}>
-										{item.name}
-									</Disclosure.Button>
-								))}
+						{/* Action */}
+						<div className='absolute hidden inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 xl:block'>
+							<div className='flex-shrink-0'>
+								<button className='bg-violet-800 px-16 py-3 drop-shadow-2xl rounded-full text-white font-medium hover:text-white hover:animate-pulse transition ease-in-out duration-200 hover:scale-110'>
+									Contact Me
+								</button>
 							</div>
-						</Disclosure.Panel>
+						</div>
 					</div>
-				</>
+					<Disclosure.Panel className='xl:hidden'>
+						<div className='space-y-1 px-2 pt-2 pb-3'>
+							{navigation.map((item) => (
+								<Disclosure.Button
+									key={item.name}
+									as='a'
+									href={item.href}
+									className='block w-100 px-10 py-2 rounded-lg text-center font-medium bg-white drop-shadow-md hover:bg-black hover:text-white transition duration-150 ease-in-out'
+									aria-current={item.current ? "page" : undefined}
+								>
+									{item.name}
+								</Disclosure.Button>
+							))}
+						</div>
+					</Disclosure.Panel>
+				</div>
 			</Disclosure>
 		</div>
 	);
--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,25 +1,35 @@
 const HeroSection = () => {
 	return (
-		<section className="absolute w-full h-screen z-40">
-			<main className="mx-4 mt-32 max-w-7xl px-4 sm:px-6 lg:mt-32 lg:px-8 xl:mt-64">
-				<div className="sm:text-left">
-					<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-						<span className="block xl:inline">I deliver</span>
-						<span className="block text-violet-800 xl:inline"> grounded</span>
-						<span className="block xl:inline"> solutions to</span>
-						<span className="block text-violet-800 xl:inline"> abstract</span>
-						<span className="block xl:inline"> problems</span>
+		<section className='absolute w-full h-screen z-40'>
+			<main className='mx-4 mt-32 max-w-7xl px-4 sm:px-6 lg:mt-32 lg:px-8 xl:mt-64'>
+				<div className='sm:text-left'>
+					<h1 className='text-5xl font-bold tracking-tight text-gray-900 md:text-6xl'>
+						<span className='inline'>I deliver</span>
+						<span className='inline text-violet-800'> grounded</span>
+						<span className='inline'> solutions to</span>
+						<span className='inline text-violet-800'> abstract</span>
+						<span className='inline'> problems</span>
 					</h1>
-					<p className="mt-3 text-base text-gray-500 sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
-					<div className="mt-5 sm:mt-8 sm:flex sm:justify-start">
-						<div className="rounded-md shadow">
-							<a href="#" className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg">
-								Get started
-							</a>
-						</div>
-						<div className="mt-3 sm:mt-0 sm:ml-3">
-							<a href="#" className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-100 px-8 py-3 text-base font-medium text-indigo-700 hover:bg-indigo-200 md:py-4 md:px-10 md:text-lg">
-								Live demo
+					<p className='mt-3 text-base text-gray-500 sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0'>
+						Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+					</p>
+					<div className='mt-5 sm:mt-8 sm:flex sm:justify-start'>
+						<div className='rounded-md shadow'>
+							<a
+								href='#'
+								className='flex w-full items-center justify-center rounded-md border border-transparent transition ease-in-out hover:scale-110 bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg'
+							>
+								Learn More
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									strokeWidth={1.5}
+									stroke='currentColor'
+									className='w-6 h-6 mx-1 transition ease-in-out hover:translate-x-2'
+								>
+									<path strokeLinecap='round' strokeLinejoin='round' d='M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3' />
+								</svg>
 							</a>
 						</div>
 					</div>
