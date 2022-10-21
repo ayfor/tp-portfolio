4137e2a40a95625fbde3db7a76512ddf99d8c78f
ayfor <josh.a.stubbington@gmail.com>
2022-10-21 01:27:10 -0400

    minor styling changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from "next";
 import Navbar from "../components/Navbar";
 import GeoOrbitSection from "../components/sections/GeoOrbitSection";
-import CurveContentSection from "../components/sections/CurveContentSection";
 import HeroSection from "../components/sections/HeroSection";
+// import CurveContentSection from "../components/sections/CurveContentSection";
 
 const Home: NextPage = () => {
 	return (
@@ -10,7 +10,6 @@ const Home: NextPage = () => {
 			<Navbar />
 			<HeroSection />
 			<GeoOrbitSection />
-			{/* <CurveContentSection /> */}
 		</>
 	);
 };
