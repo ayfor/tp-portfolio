a1b0fe1b379cd5c96b9e33389c5845dd312cc050
ayfor <josh.a.stubbington@gmail.com>
2023-04-08 00:39:50 -0400

    more skill tags, starting to lay out other cards for skills section

--- src/components/SkillTag.tsx
+++ src/components/SkillTag.tsx
@@ -1,7 +1,7 @@
 export interface SkillTagProps {
 	title: string;
 	icon: string; // src path
-	colour: "gray" | "blue" | "teal";
+	colour: "gray" | "blue" | "teal" | "white" | "red" | "navy" | "yellow" | "green" | "purple" | "pink" | "orange";
 }
 
 //TOOD: Add tooltip to show description on hover
@@ -10,11 +10,19 @@ const SkillTag = ({ title, icon, colour }: SkillTagProps) => {
 		["gray", "bg-gray-400"],
 		["blue", "bg-blue-300"],
 		["teal", "bg-cyan-800"],
+		["white", "bg-white"],
+		["red", "bg-red-400"],
+		["navy", "bg-blue-900"],
+		["yellow", "bg-yellow-400"],
+		["green", "bg-green-400"],
+		["purple", "bg-purple-400"],
+		["pink", "bg-pink-400"],
+		["orange", "bg-orange-400"],
 	]);
 
 	const colourClass = colourMap.get(colour) as string;
 
-	const divClass = `flex flex-row items-center justify-center pl-1 pr-2 py-1 m-1 text-white text-sm ${colourClass} rounded-lg shadow-lg`;
+	const divClass = `min-w-fit flex flex-row items-center justify-center pl-1 pr-2 py-1 m-1 text-white text-sm ${colourClass} rounded-lg shadow-lg`;
 
 	return (
 		<div className={divClass}>
--- src/components/sections/ServicesSection.tsx
+++ src/components/sections/ServicesSection.tsx
@@ -11,6 +11,19 @@ const WebDevelopmentSkillTags: SkillTagProps[] = [
 	{ title: "Electron", icon: "/skill-icons/electron.png", colour: "blue" },
 ];
 
+const ThreeDModelingSkillTags: SkillTagProps[] = [
+	{ title: "Three.js", icon: "/skill-icons/three-js.png", colour: "white" },
+	{ title: "Blender", icon: "/skill-icons/blender.png", colour: "navy" },
+	{ title: "SolidWorks", icon: "/skill-icons/solidworks.png", colour: "red" },
+];
+
+const UIUXDesignSkillTags: SkillTagProps[] = [
+	{ title: "Figma", icon: "/skill-icons/figma.png", colour: "purple" },
+	{ title: "Adobe Illustrator", icon: "/skill-icons/adobe-illustrator.jpg", colour: "orange" },
+	{ title: "Adobe XD", icon: "/skill-icons/adobe-xd.png", colour: "pink" },
+	{ title: "Adobe Photoshop", icon: "/skill-icons/adobe-photoshop.png", colour: "blue" },
+];
+
 /** */
 const ServicesSection = () => {
 	const servicesModelAnimation = (obj: any) => {
@@ -22,25 +35,11 @@ const ServicesSection = () => {
 			<div className='flex flex-col items-center h-full p-12'>
 				<h1 className='text-white font-display text-4xl'>Find out what I can do for you...</h1>
 
-				<div className='flex flex-col items-center justify-center text-gray-700 md:p-20'>
-					<div className='flex flex-wrap items-center justify-center w-full max-w-4xl'>
+				<div className='w-full flex flex-col items-center justify-center text-gray-700 md:p-20'>
+					<div className='flex flex-wrap items-center justify-center w-full'>
 						{/* Left Card */}
-						<div className='flex flex-col flex-grow mt-8 overflow-hidden bg-white rounded-lg shadow-lg'>
-							<div className='flex flex-col items-center p-10 bg-gray-200'>
-								<span className='font-semibold'>UI Design</span>
-								<div className='flex items-center'>
-									<span className='text-3xl'>$</span>
-									<span className='text-5xl font-bold'>20</span>
-									<span className='text-2xl text-gray-500'>/mo</span>
-								</div>
-							</div>
-							<div className='flex px-10 pb-10 justfy-center'>
-								<button className='flex items-center justify-center w-full h-12 px-6 text-sm uppercase bg-gray-200 rounded-lg'>Join now</button>
-							</div>
-						</div>
-						{/* Middle Card */}
-						<div className='z-10 flex flex-col mt-8 overflow-hidden transform md:scale-110'>
-							<div className='flex flex-col items-center p-10'>
+						<div className='max-w-md z-10 flex flex-col mt-8 overflow-hidden transform'>
+							<div className='flex flex-col items-center px-10 pt-10 pb-6'>
 								<div className='m-2p-5 max-w-xs h-96'>
 									<Canvas
 										camera={{
@@ -52,36 +51,89 @@ const ServicesSection = () => {
 									>
 										<pointLight position={[10, 10, 10]} />
 										<ambientLight intensity={0.25} />
-										<GltfModel key={"web-dev-model"} position={[0, -1, -0.25]} modelPath='/models/web-dev.glb' refAnimationFn={servicesModelAnimation} />
+										<GltfModel key={"web-dev-model"} position={[-0.5, -2, 0.75]} modelPath='/models/web-dev.glb' refAnimationFn={servicesModelAnimation} />
+									</Canvas>
+								</div>
+								<span className='font-semibold text-white text-3xl'>UI / UX Design</span>
+							</div>
+							<div className='flex flex-col items-center px-10 pb-10 justfy-center '>
+								{/* Web Dev Skill List */}
+								<div className='flex justify-center flex-row flex-wrap py-2'>
+									{UIUXDesignSkillTags.map(({ title, icon, colour }, index) => (
+										<SkillTag key={`${index}-uiuxst`} title={title} icon={icon} colour={colour} />
+									))}
+								</div>
+
+								<button className='group flex items-center justify-center w-fit h-12 px-4 text-sm uppercase rounded-full ring-inset ring-4 ring-white text-white transition ease-in-out hover:scale-105'>
+									<span className='pl-6'>Learn More</span>
+									<ArrowRightIcon className='w-6 h-6 ml-2 transition ease-in-out group-hover:translate-x-2' aria-hidden='true' />
+								</button>
+							</div>
+						</div>
+						{/* Middle Card */}
+						<div className='max-w-md z-10 flex flex-col mt-8 overflow-hidden transform md:scale-125'>
+							<div className='flex flex-col items-center px-10 pt-10 pb-6'>
+								<div className='m-2p-5 max-w-xs h-96'>
+									<Canvas
+										camera={{
+											near: 0.1,
+											far: 1000,
+											zoom: 1.1,
+											position: [0, 0, 3],
+										}}
+									>
+										<pointLight position={[10, 10, 10]} />
+										<ambientLight intensity={0.25} />
+										<GltfModel key={"web-dev-model"} position={[-0.5, -2, 0.75]} modelPath='/models/web-dev.glb' refAnimationFn={servicesModelAnimation} />
 									</Canvas>
 								</div>
 								<span className='font-semibold text-white text-3xl'>Web Development</span>
 							</div>
 							<div className='flex flex-col items-center px-10 pb-10 justfy-center '>
-								<div className='flex flex-row'>
+								{/* Web Dev Skill List */}
+								<div className='flex justify-center flex-row flex-wrap py-2'>
 									{WebDevelopmentSkillTags.map(({ title, icon, colour }, index) => (
-										<SkillTag title={title} icon={icon} colour={colour} />
+										<SkillTag key={`${index}-wdst`} title={title} icon={icon} colour={colour} />
 									))}
 								</div>
 
-								<button className='group flex items-center justify-center w-fit h-12 px-4 text-sm uppercase rounded-full ring-inset ring-4 ring-white text-white'>
+								<button className='group flex items-center justify-center w-fit h-12 px-4 text-sm uppercase rounded-full ring-inset ring-4 ring-white text-white transition ease-in-out hover:scale-105'>
 									<span className='pl-6'>Learn More</span>
 									<ArrowRightIcon className='w-6 h-6 ml-2 transition ease-in-out group-hover:translate-x-2' aria-hidden='true' />
 								</button>
 							</div>
 						</div>
 						{/* Right Card */}
-						<div className='flex flex-col flex-grow overflow-hidden bg-white rounded-lg shadow-lg mt-8'>
-							<div className='flex flex-col items-center p-10 bg-gray-200'>
-								<span className='font-semibold'>3D Modeling</span>
-								<div className='flex items-center'>
-									<span className='text-3xl'>$</span>
-									<span className='text-5xl font-bold'>99</span>
-									<span className='text-2xl text-gray-500'>/mo</span>
+						<div className='max-w-md z-10 flex flex-col mt-8 overflow-hidden transform'>
+							<div className='flex flex-col items-center px-10 pt-10 pb-6'>
+								<div className='m-2p-5 max-w-xs h-96'>
+									<Canvas
+										camera={{
+											near: 0.1,
+											far: 1000,
+											zoom: 1.1,
+											position: [0, 0, 3],
+										}}
+									>
+										<pointLight position={[10, 10, 10]} />
+										<ambientLight intensity={0.25} />
+										<GltfModel key={"web-dev-model"} position={[-0.5, -2, 0.75]} modelPath='/models/web-dev.glb' refAnimationFn={servicesModelAnimation} />
+									</Canvas>
 								</div>
+								<span className='font-semibold text-white text-3xl'>3D Modelling</span>
 							</div>
-							<div className='flex px-10 pb-10 justfy-center'>
-								<button className='flex flex-column items-center justify-center w-full h-12 px-6 text-sm uppercase bg-gray-200 rounded-lg'>Join now</button>
+							<div className='flex flex-col items-center px-10 pb-10 justfy-center '>
+								{/* Web Dev Skill List */}
+								<div className='flex justify-center flex-row flex-wrap py-2'>
+									{ThreeDModelingSkillTags.map(({ title, icon, colour }, index) => (
+										<SkillTag key={`${index}-wdst`} title={title} icon={icon} colour={colour} />
+									))}
+								</div>
+
+								<button className='group flex items-center justify-center w-fit h-12 px-4 text-sm uppercase rounded-full ring-inset ring-4 ring-white text-white transition ease-in-out hover:scale-105'>
+									<span className='pl-6'>Learn More</span>
+									<ArrowRightIcon className='w-6 h-6 ml-2 transition ease-in-out group-hover:translate-x-2' aria-hidden='true' />
+								</button>
 							</div>
 						</div>
 					</div>
