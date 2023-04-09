dfd0941ac43d31b1c9024c0966ad7b193505b2c5
ayfor <josh.a.stubbington@gmail.com>
2023-04-09 00:11:11 -0400

    removed models from services section, starting redesign of services section

--- src/components/sections/ServicesSection.tsx
+++ src/components/sections/ServicesSection.tsx
@@ -9,11 +9,13 @@ const WebDevelopmentSkillTags: SkillTagProps[] = [
 	{ title: "React", icon: "/skill-icons/react.png", colour: "teal" },
 	{ title: "Next.js", icon: "/skill-icons/next-js.png", colour: "gray" },
 	{ title: "Electron", icon: "/skill-icons/electron.png", colour: "blue" },
+	{ title: "Electron", icon: "/skill-icons/electron.png", colour: "blue" },
 ];
 
 const ThreeDModelingSkillTags: SkillTagProps[] = [
 	{ title: "Three.js", icon: "/skill-icons/three-js.png", colour: "white" },
 	{ title: "Blender", icon: "/skill-icons/blender.png", colour: "navy" },
+	{ title: "Verge3D", icon: "/skill-icons/verge3d.png", colour: "teal" },
 	{ title: "SolidWorks", icon: "/skill-icons/solidworks.png", colour: "red" },
 ];
 
@@ -45,14 +47,15 @@ const ServicesSection = () => {
 	return (
 		<div className='curve-section border-solid border-primary border-2 relative w-full min-h-fit h-full bg-primary'>
 			<div className='flex flex-col items-center h-full p-12'>
-				<h1 className='text-white font-display text-4xl'>Find out what I can do for you...</h1>
+				<h1 className='text-white font-display text-5xl'>Services</h1>
+				<h1 className='text-white font-subtitle '>Find out what I can do for you...</h1>
 
 				<div className='w-full flex flex-col items-center justify-center text-gray-700 md:p-20'>
-					<div className='flex flex-wrap align-top justify-center w-full'>
+					<div className='flex flex-wrap align-top justify-between w-full'>
 						{/* Left Card */}
-						<div className='max-w-md flex flex-col overflow-hidden'>
+						<div className='max-w-md flex flex-col overflow-hidden mt-6'>
 							<div className='flex flex-col items-center px-10 pt-10 pb-6'>
-								<div className='m-2 p-5 max-w-xs h-96'>
+								{/* <div className='m-2 p-5 max-w-xs h-96'>
 									<Canvas
 										camera={{
 											near: 0.1,
@@ -65,7 +68,7 @@ const ServicesSection = () => {
 										<ambientLight intensity={0.25} />
 										<GltfModel key={"uiux-model"} position={[0, 0, 1.25]} modelPath='/models/pencil.glb' refAnimationFn={uiuxAnimation} />
 									</Canvas>
-								</div>
+								</div> */}
 								<span className='font-semibold text-white text-3xl'>UI / UX Design</span>
 							</div>
 							<div className='max-w-md flex flex-col items-center px-10 pb-10 justfy-center '>
@@ -83,9 +86,9 @@ const ServicesSection = () => {
 							</div>
 						</div>
 						{/* Middle Card */}
-						<div className='max-w-md flex flex-col overflow-hidden mt-8 scale-110'>
+						<div className='max-w-md flex flex-col overflow-hidden'>
 							<div className='flex flex-col items-center px-10 pt-10 pb-6 '>
-								<div className='m-2 p-5 max-w-xs h-96'>
+								{/* <div className='m-2 p-5 max-w-xs h-96'>
 									<Canvas
 										camera={{
 											near: 0.1,
@@ -98,7 +101,7 @@ const ServicesSection = () => {
 										<ambientLight intensity={0.25} />
 										<GltfModel key={"web-dev-model"} position={[0, 0, -3]} modelPath='/models/web-dev.glb' refAnimationFn={webDevAnimation} />
 									</Canvas>
-								</div>
+								</div> */}
 								<span className='font-semibold text-white text-3xl'>Web Development</span>
 							</div>
 							<div className='max-w-md flex flex-col items-center px-10 pb-10 justfy-center '>
@@ -116,9 +119,9 @@ const ServicesSection = () => {
 							</div>
 						</div>
 						{/* Right Card */}
-						<div className='max-w-md flex flex-col overflow-hidden'>
+						<div className='max-w-md flex flex-col overflow-hidden mt-6'>
 							<div className='flex flex-col items-center px-10 pt-10 pb-6'>
-								<div className='m-2 p-5 max-w-xs h-96'>
+								{/* <div className='m-2 p-5 max-w-xs h-96'>
 									<Canvas
 										camera={{
 											near: 0.1,
@@ -131,7 +134,7 @@ const ServicesSection = () => {
 										<ambientLight intensity={0.25} />
 										<GltfModel key={"3d-showcase-model"} position={[0, 0, 2]} modelPath='/models/showcase-temple.glb' refAnimationFn={threeDAnimation} />
 									</Canvas>
-								</div>
+								</div> */}
 								<span className='font-semibold text-white text-3xl'>3D Modelling</span>
 							</div>
 							<div className='flex flex-col items-center px-10 pb-10 justfy-center '>
