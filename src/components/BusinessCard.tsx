bdd4c835894e070e66f4320ada2c6e143add305f
ayfor <josh.a.stubbington@gmail.com>
2023-04-24 01:35:21 -0400

    alt logo for navbar, moving navbar items to mid, prepping for social links for navbar

--- src/components/BusinessCard.tsx
+++ src/components/BusinessCard.tsx
@@ -8,9 +8,13 @@ type NavbarItem = {
 	current: boolean;
 };
 
-type NavbarItems = NavbarItem[];
+type LogoBarItem = {
+	name: string;
+	source: string;
+	alt: string;
+};
 
-const navigation: NavbarItems = [
+const navigation: NavbarItem[] = [
 	{ name: "about", href: "#", current: true },
 	{ name: "services", href: "#", current: false },
 	{ name: "my work", href: "#", current: false },
@@ -18,37 +22,33 @@ const navigation: NavbarItems = [
 
 const BusinessCard = () => {
 	return (
-		<Disclosure as='div' className='sticky top-0'>
-			<div className='mx-auto max-w-full px-2 sm:px-6 lg:px-8'>
-				<div className='relative flex h-16 items-center justify-between my-2'>
+		<Disclosure as="div" className="sticky top-0">
+			<div className="mx-auto max-w-full px-2 sm:px-6 lg:px-8">
+				<div className="relative flex h-16 items-center justify-between my-2">
 					{/* PROFILE CARD */}
 					<ProfileCard />
 
 					{/* Default Menu */}
-					<div className='flex flex-1 items-center justify-center'>
-						<div className='hidden w-full flex-row justify-end items-center sm:mx-6 2xl:flex'>
-							<div className='w-1/4 flex justify-between space-x-4'>
+					<div className="flex flex-1 items-center justify-center">
+						<div className="hidden w-full flex-row justify-center items-center sm:mx-6 2xl:flex">
+							<div className="w-1/4 flex justify-between space-x-4">
 								{navigation.map((item) => (
-									<button
-										key={item.name}
-										className='text-primary font-medium bg-white px-4 py-2 rounded-lg w-1/4 transition duration-150 ease-in-out hover:scale-125'
-										aria-current={item.current ? "page" : undefined}
-									>
-										{item.name}
+									<button key={item.name} className="text-primary font-display font-bold bg-transparent px-4 py-2 rounded-lg w-1/4 transition duration-150 ease-in-out hover:scale-125" aria-current={item.current ? "page" : undefined}>
+										{item.name}.
 									</button>
 								))}
 							</div>
 						</div>
 					</div>
 				</div>
-				<Disclosure.Panel className='2xl:hidden'>
-					<div className='space-y-1 px-2 pt-2 pb-3'>
+				<Disclosure.Panel className="2xl:hidden">
+					<div className="space-y-1 px-2 pt-2 pb-3">
 						{navigation.map((item) => (
 							<Disclosure.Button
 								key={item.name}
-								as='a'
+								as="a"
 								href={item.href}
-								className='block w-100 min-w-full sm:w-5/12 px-10 py-2 rounded-lg text-center font-medium bg-white drop-shadow-md hover:bg-primary hover:text-white transition duration-150 ease-in-out'
+								className="block w-100 min-w-full sm:w-5/12 px-10 py-2 rounded-lg text-center font-medium bg-white drop-shadow-md hover:bg-primary hover:text-white transition duration-150 ease-in-out"
 								aria-current={item.current ? "page" : undefined}
 							>
 								{item.name}
